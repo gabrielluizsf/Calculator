e73139d4609ff9e33ff210e7bde48c33e0af540c
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-05-27 08:16:58 -0300

    how to create calcukator Dark & Light

---
+++ lib/utils/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+/// Dark Colors
+class DarkColors {
+  static const scaffoldBgColor = Color(0x0000);
+  static const sheetBgColor = Color(0x000);
+  static const btnBgColor = Color.fromARGB(255, 33, 0, 42);
+  static const leftOperatorColor = Color.fromARGB(255, 1, 157, 128);
+  static const operatorColor = Color(0xffEB6666);
+}
+
+/// Light Colors
+class LightColors {
+  static const scaffoldBgColor = Color(0xffF9F9F9);
+  static const sheetBgColor = Color(0xffF9F9F9);
+  static const btnBgColor = Color.fromARGB(255, 243, 243, 243);
+  static const operatorColor = Color(0xffEB6666);
+  static const leftOperatorColor = Color.fromARGB(255, 1, 157, 128);
+}
